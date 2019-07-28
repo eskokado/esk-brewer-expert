de699185428a89adf274a9ea6d0f7ebbecfa6eaa
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-28 13:57:11 -0300

    3.6 Começando o cadastro de cerveja

--- brewer/src/main/java/com/algaworks/brewer/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/CervejasController.java
@@ -1,13 +1,22 @@
 package com.algaworks.brewer;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.algaworks.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
 
-	@RequestMapping("/cervejas/novo")
+	@GetMapping("/cervejas/novo")
 	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+
+	@PostMapping("/cervejas/novo")
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println("Chamou Cadastrar ---> " + cerveja.getSku());
 		return "cerveja/CadastroCerveja";
 	}
 	
